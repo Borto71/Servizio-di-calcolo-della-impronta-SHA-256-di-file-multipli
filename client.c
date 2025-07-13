6fe3eab7fb61e690237919e9c435c5ae5ffb5237
Borto71 <mattiaborto71@gmail.com>
2025-07-13 10:52:37 +0200

    Update client.c

--- client.c
+++ client.c
@@ -1,64 +1,54 @@
-// SERVER.C
+// CLIENT.C
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <pthread.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <sys/stat.h>
-#include <openssl/sha.h>
 
 #define SERVER_FIFO "/tmp/server_fifo"
 
-void digest_file(const char *filename, uint8_t *hash); // dichiarazione funzione SHA256
-
-// Struttura richiesta ricevuta
 typedef struct {
-    char filepath[256];     // percorso del file da leggere
-    char client_fifo[256];  // percorso FIFO privata del client
+    char filepath[256];
+    char client_fifo[256];
 } request_t;
 
-// Thread handler per ogni richiesta
-void *handle_request(void *arg) {
-    request_t req = *(request_t *)arg;
-    free(arg);
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Uso: %s <percorso_file>\n", argv[0]);
+        return 1;
+    }
 
-    uint8_t hash[32];
-    digest_file(req.filepath, hash);
+    // Creazione FIFO privata
+    char client_fifo[256];
+    sprintf(client_fifo, "/tmp/client_fifo_%d", getpid());
+    mkfifo(client_fifo, 0666);
 
-    char char_hash[65];
-    for (int i = 0; i < 32; i++)
-        sprintf(char_hash + (i * 2), "%02x", hash[i]);
-    char_hash[64] = '\0';
+    // Composizione della richiesta
+    request_t req;
+    strcpy(req.filepath, argv[1]);
+    strcpy(req.client_fifo, client_fifo);
 
-    // Scrive il risultato sulla FIFO privata del client
-    int fd = open(req.client_fifo, O_WRONLY);
-    if (fd >= 0) {
-        write(fd, char_hash, sizeof(char_hash));
-        close(fd);
+    // Invio al server
+    int server_fd = open(SERVER_FIFO, O_WRONLY);
+    if (server_fd < 0) {
+        perror("Errore apertura FIFO server");
+        unlink(client_fifo);
+        return 1;
+    }
+    write(server_fd, &req, sizeof(req));
+    close(server_fd);
+
+    // Attesa risposta
+    int client_fd = open(client_fifo, O_RDONLY);
+    char hash[65];
+    if (read(client_fd, hash, sizeof(hash)) > 0) {
+        printf("SHA-256 di '%s':\n%s\n", argv[1], hash);
     } else {
-        perror("Errore apertura FIFO client");
+        printf("Errore nella lettura della risposta dal server.\n");
     }
 
-    return NULL;
-}
-
-int main() {
-    // Creazione FIFO server
-    mkfifo(SERVER_FIFO, 0666);
-    printf("[SERVER] In ascolto su %s...\n", SERVER_FIFO);
-
-    int fd = open(SERVER_FIFO, O_RDONLY);
-    while (1) {
-        request_t *req = malloc(sizeof(request_t));
-        if (read(fd, req, sizeof(request_t)) > 0) {
-            pthread_t tid;
-            pthread_create(&tid, NULL, handle_request, req);
-            pthread_detach(tid); // evitiamo di dover fare join
-        }
-    }
-
-    close(fd);
-    unlink(SERVER_FIFO);
+    close(client_fd);
+    unlink(client_fifo);
     return 0;
 }
